ece2d732c1b8f1bb397557e0f7b759e4d2bf8ba3
Brandon Pereira <brandonp@criticalmass.com>
2017-11-05 21:28:16 -0700

    basic loader screen

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,21 +1,17 @@
 const requiredDependencies = [ // Dependencies which are required before app ready
-	import('./lib/animate')
+	// TODO: Turn this into an Object
+	// that way you can require like .then({react})
+	// import('dep1')
 ];
 const optionalDependencies = [ // Dependencies which can be loaded async
 	import('../styles/app.scss')
 ];
 
 Promise.all(requiredDependencies)
-	.then(([animate]) => {
-		console.info("Application Ready");
-		animate(document.querySelector('svg'), [
-			{opacity: 0},
-			{opacity: 1}
-		], {
-			duration: 1000,
-			iterations: Infinity,
-			direction: 'alternate'
-		});
+	.then(() => { // .then(([dep1, dep2]) =>
+		// all requiredDependencies imported
+		document.body.classList.add('loaded')
+		
 	})
 	.catch((err) => console.error("Failed to load dependencies.", err))
 	
