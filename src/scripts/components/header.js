248d2f8f2d83a9f1a18edead771298eaed2c327b
Brandon Pereira <brandonp@criticalmass.com>
2017-11-19 17:02:29 -0700

    began transitioning to styled components

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -1,30 +1,53 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../actions';
-import Greeting from './greeting';
-import Button from './button';
-import '../../styles/components/header.scss';
+import styled from 'styled-components';
 
+const Container = styled.header`
+  grid-row: 1 / 1;
+  grid-column: 2 / 3;
+  background-color: #2177BD;
+  background-image: linear-gradient(transparent, rgba(0,0,0,.2));
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 0.8rem;
+`;
+const Button = styled.button`
+  appearance: none;
+  background: linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.4));
+  border: 1px solid rgba(0,0,0,.6);
+  box-shadow: inset 0 1px rgba(255,255,255,.2);
+  border-radius: 5px;
+  padding: 0.8rem;
+  color: #fff;
+`;
+const Title = styled.h2`
+  flex: 1;
+  text-align: center;
+`;
 class Header extends Component {
   
   render() {
     return (
-      <header className="main">
-        <Button>Back</Button>
-        <Greeting />
-        <Button>Settings</Button>
-      </header>
+      <Container>
+        <Button onClick={this.props.backButtonClick}>Back</Button>
+        <Title>{this.props.currentList.title}</Title>
+        <Button onClick={this.props.settingsButtonClick}>Settings</Button>
+      </Container>
     )
   }
-  
+
 }
  
 export default connect(
   (state) => ({
-    navigation: state.navigation
+    navigation: state.navigation,
+    currentList: state.currentList
   }),
   (dispatch) => ({
-    click: () => dispatch(actions.click())
+    backButtonClick: () => dispatch(actions.click()),
+    settingsButtonClick: () => dispatch(actions.click())
   })
 
 )(Header)
--- src/scripts/components/logo.js
+++ src/scripts/components/logo.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import actions from '../actions';
-import '../../styles/components/logo.scss';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	grid-row: 1 / 1;
+	grid-column: 1 / 1;
+	cursor: pointer;
+	background: linear-gradient(transparent, rgba(0,0,0,.1)), #343434;
+	color: #fff;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	h1 {
+		font-size: 1.8rem;
+		font-weight: 200;
+	}
+	span {
+		font-weight: 600;
+	}
+`;
 
 class Header extends Component {
   render() {
-    return <div className="logo">
+    return <Container>
       <h1>Better<span>Do.</span></h1>
-    </div>
+    </Container>
   }
 }
  
