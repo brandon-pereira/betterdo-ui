9202ec941034110cf1f071ac3946d32bd63ca935
Brandon Pereira <brandonp@criticalmass.com>
2018-10-11 21:14:46 -0600

    added a todo

--- src/scripts/components/body.js
+++ src/scripts/components/body.js
@@ -10,11 +10,13 @@ const Container = styled.div``;
 @observer
 export default class Body extends Component {
     render() {
+        // TODO: Fix but with double rendering and keys
+        // console.log(this.props.state.currentList.tasks);
         return (
             <Container>
                 <AddTask />
                 {this.props.state.currentList.tasks.map((task, i) => {
-                    return <Task key={i}>{task.title}</Task>;
+                    return <Task key={task._id} task={task} />;
                 })}
             </Container>
         );
