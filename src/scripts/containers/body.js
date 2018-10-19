b3f3dc1fa6bcf828df93a606c1501df61b46127a
Brandon Pereira <brandonp@criticalmass.com>
2018-10-19 12:20:08 -0600

    clean up body

--- src/scripts/containers/body.js
+++ src/scripts/containers/body.js
@@ -10,14 +10,12 @@ const Container = styled.div``;
 @observer
 export default class Body extends Component {
     render() {
-        // TODO: Fix but with double rendering and keys
-        // console.log(this.props.store.currentList.tasks);
         return (
             <Container>
                 <AddTask />
-                {this.props.store.currentList.tasks.map((task, i) => {
-                    return <Task key={task._id} task={task} />;
-                })}
+                {this.props.store.currentList.tasks.map(task => (
+                    <Task key={task._id} task={task} />
+                ))}
             </Container>
         );
     }
