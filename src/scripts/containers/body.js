929922484b983b61ea069b6bddb7148f4a73ba00
Brandon Pereira <brandonp@criticalmass.com>
2018-10-21 00:46:47 -0600

    ability to reorder lists

--- src/scripts/containers/body.js
+++ src/scripts/containers/body.js
@@ -1,22 +1,58 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import styled from 'styled-components';
+import { computed } from 'mobx';
 import AddTask from '../components/addTask';
 import Task from '../components/task';
+import {
+    SortableContainer,
+    SortableElement,
+    arrayMove
+} from 'react-sortable-hoc';
 
-const Container = styled.div``;
+const SortableItem = SortableElement(({ value }) => <Task task={value} />);
+
+const SortableList = SortableContainer(({ items }) => {
+    return (
+        <div>
+            {items.map((task, index) => (
+                <SortableItem key={task._id} index={index} value={task} />
+            ))}
+        </div>
+    );
+});
 
 @inject('store')
 @observer
 export default class Body extends Component {
+    @computed
+    get currentList() {
+        return this.props.store.currentList;
+    }
+
+    onSortEnd = ({ oldIndex, newIndex }) => {
+        this.currentList.tasks = arrayMove(
+            this.currentList.tasks,
+            oldIndex,
+            newIndex
+        );
+        try {
+            this.props.store.updateList(this.currentList._id, {
+                tasks: this.currentList.tasks.map(t => t._id)
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
     render() {
         return (
-            <Container>
+            <div>
                 <AddTask />
-                {this.props.store.currentList.tasks.map(task => (
-                    <Task key={task._id} task={task} />
-                ))}
-            </Container>
+                <SortableList
+                    items={this.currentList.tasks.map(task => task)}
+                    onSortEnd={this.onSortEnd}
+                />
+            </div>
         );
     }
 }
