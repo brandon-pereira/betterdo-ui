e89d738ecf963a665fc17987d719721b1dcc7414
Brandon Pereira <brandonp@criticalmass.com>
2018-11-14 21:58:31 -0700

    work to mobile

--- src/scripts/containers/container.js
+++ src/scripts/containers/container.js
@@ -1,12 +1,19 @@
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { QUERIES } from '../constants';
+import { observer, inject } from 'mobx-react';
 
-export default styled.div`
+const Div = styled.div`
     width: 100vw;
     height: 100vh;
     display: grid;
     grid-template-columns: 0px 1fr;
     grid-template-rows: 4rem 1fr;
+    ${props =>
+        props.mobileNavVisible &&
+        `
+          grid-template-rows: 4rem 80% 1fr;  
+    `}
     @media ${QUERIES.medium} {
         grid-template-columns: 10rem 1fr;
     }
@@ -15,3 +22,17 @@ export default styled.div`
         grid-template-columns: 14rem 1fr;
     }
 `;
+
+@inject('store')
+@observer
+class Container extends Component {
+    render() {
+        return (
+            <Div mobileNavVisible={this.props.store.modalVisibility.listsView}>
+                {this.props.children}
+            </Div>
+        );
+    }
+}
+
+export default styled(Container)``;
--- src/scripts/containers/navigation.js
+++ src/scripts/containers/navigation.js
@@ -6,22 +6,26 @@ import ListItem from '../components/list';
 import AddListModal from './addListModal';
 import { QUERIES } from '../constants';
 
+const NavigationModalOverlay = styled.div`
+    background: rgba(0, 0, 0, 0.5);
+    position: absolute;
+    top: 100%;
+    left: 0;
+    right: 0;
+    height: 20%;
+`;
 const Container = styled.nav`
     background: #202020;
     display: flex;
     flex-direction: column;
     box-shadow: inset 0 1px rgba(255, 255, 255, 0.15);
-    overflow: hidden;
     transform: translateY(-100%);
     transition: transform .2s;
      ${props =>
          props.visibleOnMobile &&
          `
-        position: fixed;
-        top: 4rem;
-        left: 0;
-        right: 0;
-        bottom: 20%;
+        grid-row: 2;
+        grid-column: 2;
         transform: translateY(0%);
     `}
     @media ${QUERIES.medium} {
@@ -66,6 +70,11 @@ class Navigation extends Component {
                         newList
                     />
                 </ListsContainer>
+                <NavigationModalOverlay
+                    onClick={() => {
+                        this.props.store.modalVisibility.listsView = false;
+                    }}
+                />
                 <AddListModal />
             </Container>
         );
