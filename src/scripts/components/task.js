39ba45b7978d7cb64531132e438d28e4c4a2cab1
Brandon Pereira <brandonp@criticalmass.com>
2018-12-21 15:44:45 -0700

    high priority flag

--- src/scripts/components/task.js
+++ src/scripts/components/task.js
@@ -17,6 +17,7 @@ const Checkbox = styled.input`
     outline: none;
     cursor: pointer;
     flex-shrink: 0;
+    margin: 1.2rem 1rem;
     &:before {
         content: '';
         border-radius: 50%;
@@ -31,25 +32,56 @@ const Checkbox = styled.input`
         transform: scale(1);
     }
 `;
+const HighPriorityFlag = styled.div`
+    align-self: flex-start;
+    height: 2rem;
+    width: 1.6rem;
+    background: linear-gradient(#e21d1d, #c11010);
+    margin: -1px 1rem 0 0;
+    position: relative;
+    filter: drop-shadow(1px 1px #840000);
+    &:before,
+    &:after {
+        content: '';
+        position: absolute;
+        top: calc(100% - 1px);
+        left: 0;
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 0.8rem 0.8rem 0 0;
+        border-color: #c11010 transparent transparent transparent;
+    }
+    &:after {
+        left: auto;
+        right: 0;
+        border-width: 0 0.8rem 0.8rem 0;
+        border-color: transparent #c11010 transparent transparent;
+    }
+`;
 const Container = styled.div`
     background: linear-gradient(#fff, #eee);
     margin: 0.5rem 1rem;
     border-radius: 5px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.2), inset 0 -1px #fff;
-    padding: 1rem;
     display: flex;
     align-items: center;
-    ${props => props.isLoading && `
+    ${props =>
+        props.isLoading &&
+        `
         opacity: 0.5;
         pointer-events: none;
     `}
+    ${Loader} {
+        margin: 1rem;
+    }
 `;
 const Title = styled.span`
     flex: 1;
     text-overflow: ellipsis;
     overflow: hidden;
     user-select: none;
-    margin-left: 1rem;
+    margin: 1rem 1rem 1rem 0;
 `;
 @inject('store')
 @observer
@@ -68,17 +100,22 @@ class Task extends Component {
     render() {
         const { task } = this.props;
         return (
-            <Container isLoading={task.isLoading} onClick={this.edit.bind(this)}>
-                {task.isLoading ? 
-                    <Loader color="#202020" size="1.7rem" loading={true} /> : 
+            <Container
+                isLoading={task.isLoading}
+                onClick={this.edit.bind(this)}
+            >
+                {task.isLoading ? (
+                    <Loader color="#202020" size="1.7rem" loading={true} />
+                ) : (
                     <Checkbox
                         type="checkbox"
                         onClick={e => e.stopPropagation()}
                         onChange={this.toggleCompleted.bind(this)}
                         checked={task.isCompleted}
                     />
-                }
+                )}
                 <Title>{task.title}</Title>
+                {task.priority === 'high' && <HighPriorityFlag />}
             </Container>
         );
     }
