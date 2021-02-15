069719fcb9a7fcc38e4abe13470c20716220ecac
Brandon Pereira <brandonp@criticalmass.com>
2021-02-15 16:36:02 -0700

    build relative

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.min.js',
-        publicPath: '/'
+        publicPath: isProduction ? '/app' : '/'
     },
     devServer: {
         hot: true,
