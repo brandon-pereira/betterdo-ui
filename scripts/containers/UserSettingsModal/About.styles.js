6c5aaf6f1921b8c62ebdc690928d4a39834dc9b0
Brandon Pereira <brandonp@criticalmass.com>
2021-09-30 10:38:20 -0600

    fix overflow-x

--- scripts/containers/UserSettingsModal/About.styles.js
+++ scripts/containers/UserSettingsModal/About.styles.js
@@ -26,7 +26,7 @@ export const ChangeLog = styled.div`
     text-align: left;
     box-sizing: border-box;
     padding: 0.5rem 1rem;
-    overflow: scroll;
+    overflow-y: scroll;
     border: 2px solid #ddd;
     border-radius: 1rem;
     margin-top: 2rem;
