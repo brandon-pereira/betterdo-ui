d1ab9f90aefbee27d82ebf1580f1084b13558e84
Brandon Pereira <brandonp@criticalmass.com>
2021-10-01 13:37:04 -0600

    disable service worker in dev

--- scripts/utilities/ServiceWorkerRegister.js
+++ scripts/utilities/ServiceWorkerRegister.js
@@ -1,6 +1,10 @@
 import { Workbox } from 'workbox-window';
+import.meta.hot;
 
-if ('serviceWorker' in navigator) {
+if (
+    'serviceWorker' in navigator &&
+    __SNOWPACK_ENV__.NODE_ENV === 'production'
+) {
     const wb = new Workbox('service-worker.js');
     wb.register();
 }
