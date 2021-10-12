1de8814a757d6b2ae89dc7c19cc64f9b8969e24e
Brandon Pereira <brandonp@criticalmass.com>
2021-10-11 22:14:00 -0600

    upgrade selector to hooks

--- scripts/components/Selector/Selector.jsx
+++ scripts/components/Selector/Selector.jsx
@@ -1,76 +1,35 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { useCallback, useState } from 'react';
 
-const SelectorContainer = styled.div`
-    display: flex;
-    border-radius: 3px;
-    margin-bottom: 1rem;
-`;
-const Selection = styled.button`
-    flex: 1;
-    appearance: none;
-    border: none;
-    margin: 0;
-    background: ${({ theme }) => theme.colors.forms.selector.background};
-    color: ${({ theme }) => theme.colors.forms.selector.color};
-    box-shadow: ${({ theme }) => theme.colors.forms.selector.boxShadow};
-    padding: 0.75rem 1rem;
-    font: inherit;
-    outline: none;
-    &:first-of-type {
-        border-radius: 5px 0 0 5px;
-    }
-    &:last-of-type {
-        border-right: none;
-        border-radius: 0 5px 5px 0;
-    }
-    &:focus-visible {
-        background-image: linear-gradient(#ddd, #fff);
-    }
-    ${props =>
-        props.selected &&
-        `
-        background-color: ${props.theme.colors.general.blue};
-        background-image: linear-gradient(transparent, rgba(0,0,0,.3));
-        box-shadow: inset 0 0 0 1px rgba(0,0,0,0.9), inset 0 -2px rgba(255,255,255,.3);
-        text-shadow: 1px 1px rgba(0,0,0,.9);
-        position: relative;
-        z-index: 1;
-        color: #fff;
-        &:focus-visible {
-            background-image: linear-gradient(rgba(0,0,0,.3), transparent);
-        }
-    `}
-`;
-class Selector extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { selectedValue: props.value || props.values[0].value };
-        this.handleChange = this.handleChange.bind(this);
-    }
+import { Container, Selection } from './Selector.styles.js';
 
-    handleChange(e, value) {
-        this.setState({ selectedValue: value });
-        if (this.props.onSelect) {
-            this.props.onSelect(value);
-        }
-    }
+function Selector({ value, values, onSelect }) {
+    const [selectedValue, setSelectedValue] = useState(
+        value || values[0].value
+    );
 
-    render() {
-        return (
-            <SelectorContainer>
-                {this.props.values.map(value => (
-                    <Selection
-                        onClick={e => this.handleChange(e, value.value)}
-                        key={value.value}
-                        selected={value.value === this.state.selectedValue}
-                    >
-                        {value.label}
-                    </Selection>
-                ))}
-            </SelectorContainer>
-        );
-    }
+    const onChange = useCallback(
+        (e, value) => {
+            setSelectedValue(value);
+            if (onSelect) {
+                onSelect(value);
+            }
+        },
+        [onSelect]
+    );
+
+    return (
+        <Container>
+            {values.map(value => (
+                <Selection
+                    onClick={e => onChange(e, value.value)}
+                    key={value.value}
+                    selected={value.value === selectedValue}
+                >
+                    {value.label}
+                </Selection>
+            ))}
+        </Container>
+    );
 }
 
 export default Selector;
