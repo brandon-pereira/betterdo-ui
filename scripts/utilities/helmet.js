599ef3a0e91a05621f3670cbe522e078b642513e
Brandon Pereira <brandonp@criticalmass.com>
2021-12-28 16:01:51 -0700

    drop unused dependencies and files

--- scripts/utilities/helmet.js
+++
@@ -1,7 +0,0 @@
-export const setThemeColor = color => {
-    if (typeof document !== undefined) {
-        document
-            .querySelector('meta[name="theme-color"]')
-            .setAttribute('content', color);
-    }
-};
