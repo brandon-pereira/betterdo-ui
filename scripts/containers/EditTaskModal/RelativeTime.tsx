cbcb6687dc18be8a19d7713b94ee74136248c1cb
Brandon Pereira <brandonp@criticalmass.com>
2022-02-21 21:34:47 -0700

    better date parsing

--- scripts/containers/EditTaskModal/RelativeTime.tsx
+++ scripts/containers/EditTaskModal/RelativeTime.tsx
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-type Unit = { unit: Intl.RelativeTimeFormatUnit; ms: number };
-
-const units: Unit[] = [
-    { unit: 'year', ms: 31536000000 },
-    { unit: 'month', ms: 2628000000 },
-    { unit: 'day', ms: 86400000 },
-    { unit: 'hour', ms: 3600000 },
-    { unit: 'minute', ms: 60000 }
-];
-const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+import { formatRelative } from 'date-fns';
 
 export default function RelativeTime({ date }: { date: Date }) {
     const [relativeTime, setRelativeTime] = useState(
@@ -31,21 +22,7 @@ export default function RelativeTime({ date }: { date: Date }) {
  * @param relative  - the relative dateTime, generally is in the past or future
  * @param pivot     - the dateTime of reference, generally is the current time
  */
-export function relativeTimeFromDates(relative: Date, pivot = new Date()) {
+function relativeTimeFromDates(relative: Date, pivot = new Date()) {
     if (!relative) return '';
-    const elapsed = relative.getTime() - pivot.getTime();
-    return relativeTimeFromElapsed(elapsed);
-}
-
-/**
- * Get language-sensitive relative time message from elapsed time.
- * @param elapsed   - the elapsed time in milliseconds
- */
-export function relativeTimeFromElapsed(elapsed: number) {
-    for (const { unit, ms } of units) {
-        if (Math.abs(elapsed) >= ms) {
-            return rtf.format(Math.round(elapsed / ms), unit);
-        }
-    }
-    return 'a few seconds ago';
+    return formatRelative(relative, pivot);
 }
